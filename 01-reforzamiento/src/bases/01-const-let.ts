25e3d8a856b357e7767786cb1bb2b7cf6665bbe3
ander-ortiz <ortizone620@gmail.com>
2025-09-05 14:39:49 -0600

    sección 4 "Refuerzo de ts"

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,8 @@
+const firtname:string = 'Ander',
+    lastname:string = 'Ortiz',
+    constainsLetterZ = lastname.includes('z');
+
+let diceNumber:number = 5;
+diceNumber = 3;
+
+console.log({constainsLetterZ, diceNumber});
