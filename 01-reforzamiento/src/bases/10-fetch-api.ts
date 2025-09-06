ec3e0dbef1161b830dcb88403fda3e6a28e4840f
ander-ortiz <ortizone620@gmail.com>
2025-09-06 14:38:01 -0600

    fech-api

---
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -0,0 +1,25 @@
+import type { GifphyRandomResponse } from "../data/Giphi.data";
+
+const API_KEY = 'PiaqfidnwvfWdjkl4BQ7DBGxuigxVGg6';
+
+const myRequest = fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+);
+
+const createImageInsideDOM = async(url:string) => {
+    const imgElement = document.createElement('img');
+        imgElement.src = url;
+
+        document.body.append(imgElement);
+}
+
+
+myRequest
+    .then((response) => response.json())
+    .then(async({data}:GifphyRandomResponse) => {
+        const imageUrl = data.images.original.url;
+        await createImageInsideDOM(imageUrl)
+    })
+    .catch((err) => {
+        console.error(err);
+    })
