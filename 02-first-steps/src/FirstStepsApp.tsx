a914a43d86e058ca516b8f2199dffe00ab9e9711
ander-ortiz <ortizone620@gmail.com>
2025-09-07 21:26:19 -0600

    Mi primer componente + tarea

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+
+export const FirstStepsApp = () => {
+    return (
+        <>
+            <h1>Hello world!!!</h1>
+            <p>Esto es un parrafo</p>
+
+            <button>Click me</button>
+
+            <div>
+                <h2>Hola dentro de un div</h2>
+            </div>
+        </>
+    )
+}
---
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -0,0 +1,8 @@
+export function MyAwasomeApp () {
+    return (
+        <>
+            <h1>Ander</h1>
+            <h3>Ortiz</h3>
+        </>
+    )
+}
