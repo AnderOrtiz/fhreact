930be8444e75eea50a32f4df1a6c4335914d057a
Anderson Ortiz <ortizone620@gmail.com>
2026-01-08 14:45:31 -0600

    feat: add @testing-library/jest-dom for enhanced testing capabilities and update package dependencies

--- src/counter/components/MycounterApp2.test.tsx
+++ src/counter/components/MycounterApp2.test.tsx
@@ -1,11 +1,10 @@
-import { screen, render, fireEvent } from "@testing-library/react";
-import { describe, expect, test, vi } from "vitest"
-import MycounterApp from "./MycounterApp";
+import { screen, render, fireEvent } from '@testing-library/react'
+import { describe, expect, test, vi } from 'vitest'
+import MycounterApp from './MycounterApp'
 
 const handleAddMock = vi.fn()
 const handleSubtractMock = vi.fn()
 const handleResetMock = vi.fn()
-
 
 vi.mock('../hooks/useCounter', () => ({
     default: () => ({
@@ -13,31 +12,29 @@ vi.mock('../hooks/useCounter', () => ({
         handleAdd: handleAddMock,
         handleSubtract: handleSubtractMock,
         handleReset: handleResetMock,
-        // El fn sirver para ver información de la ejecución
     }),
-}));
+}))
 
 describe('MyCounterApp', () => {
-    test('should render the componenet', () => {
+    test('should render the component', async () => {
         render(<MycounterApp />)
-        // screen.debug();
 
-        expect(screen.getByRole('heading', { level: 1 }).innerHTML).toContain(`counter: 40`)
+        const heading = await screen.findByRole('heading', { level: 1 })
+        expect(heading).toHaveTextContent('counter: 40')
 
-        expect(screen.getByRole('button', { name: '+1' })).toBeDefined();
-        expect(screen.getByRole('button', { name: '-1' })).toBeDefined();
-        expect(screen.getByRole('button', { name: 'Reset' })).toBeDefined();
+        expect(await screen.findByRole('button', { name: '+1' })).toBeInTheDocument()
+        expect(await screen.findByRole('button', { name: '-1' })).toBeInTheDocument()
+        expect(await screen.findByRole('button', { name: 'Reset' })).toBeInTheDocument()
     })
 
-    test('should call handleAdd if button is clicked', () => {
+    test('should call handleAdd if button is clicked', async () => {
         render(<MycounterApp />)
-        const button = screen.getByRole('button', { name: '+1' })
-        fireEvent.click(button);
 
-        expect(handleAddMock).toHaveBeenCalled();
-        expect(handleAddMock).toHaveBeenCalledTimes(1);
-        expect(handleSubtractMock).not.toHaveBeenCalled();
-        expect(handleResetMock).not.toHaveBeenCalled();
+        const button = await screen.findByRole('button', { name: '+1' })
+        fireEvent.click(button)
+
+        expect(handleAddMock).toHaveBeenCalledTimes(1)
+        expect(handleSubtractMock).not.toHaveBeenCalled()
+        expect(handleResetMock).not.toHaveBeenCalled()
     })
-
-});
+})
