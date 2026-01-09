d00579c960358d2c17a409895fc559fc1699c094
Anderson Ortiz <ortizone620@gmail.com>
2026-01-09 00:04:19 -0600

    fix: enable test execution in Dockerfile and improve MycounterApp2 tests for accuracy

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
     test: {
         environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
         globals: true,
     },
 })
+
+
+// npm install -D @testing-library/jest-dom
